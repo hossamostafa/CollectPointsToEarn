51633d0c08c1e858177b738fc90d20575c09df4c
hossamostafa <hossamostafa877@yahoo.com>
2021-11-25 16:11:16 +0200

    update activity_main.xml and Add border2.xml and Add border3.xml
    (cherry picked from commit 5cb3fff77eaea26a15e61e411ebf290115ce6320)

--- app/src/main/java/com/koshy/collectpoints/MainActivity.java
+++ app/src/main/java/com/koshy/collectpoints/MainActivity.java
@@ -1,7 +1,6 @@
 package com.koshy.collectpoints;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
 
 
     }
