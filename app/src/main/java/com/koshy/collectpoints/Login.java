8a9dccb2ea02dc85c6c5ab57724330ea29b8b0da
nabeel <abomaher.algefary@gmail.com>
2021-11-29 13:05:33 +0200

    I am adding many checks for Register.java and Login.java and adding ForgetPassword

--- app/src/main/java/com/koshy/collectpoints/Login.java
+++ app/src/main/java/com/koshy/collectpoints/Login.java
@@ -2,13 +2,69 @@ package com.koshy.collectpoints;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-
+   EditText UserNAmeField , PasswordField;
+   Button login ;
+   TextView register_now , forget_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
+        ClickLogin();
+        ClickRegisterNow();
+        ClickForgetPassword();
+    }
+
+    private void ClickForgetPassword() {
+    forget_password.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(),ForgetPassword.class);
+            startActivity(intent);
+        }
+    });
+    }
+
+    private void ClickRegisterNow() {
+    register_now.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(),Register.class);
+            startActivity(intent);
+        }
+    });
+    }
+
+    private void ClickLogin() {
+    login.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            checkable();
+        }
+    });
+    }
+
+    private void checkable() {
+    if(UserNAmeField.getText().toString().isEmpty())
+        Toast.makeText(getApplicationContext(),"UserName is notFound",Toast.LENGTH_LONG).show();
+    else if (PasswordField.getText().toString().isEmpty())
+        Toast.makeText(getApplicationContext(),"Password is notFound",Toast.LENGTH_LONG).show();
+    }
+
+    private void init(){
+        UserNAmeField = findViewById(R.id.email_username_field);
+        PasswordField = findViewById(R.id.password_log);
+        login = findViewById(R.id.btn_log);
+        register_now = findViewById(R.id.register_now);
+        forget_password = findViewById(R.id.forget_pass);
     }
 }
--- app/src/main/java/com/koshy/collectpoints/Register.java
+++ app/src/main/java/com/koshy/collectpoints/Register.java
@@ -3,12 +3,80 @@ package com.koshy.collectpoints;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
-    @Override
+   EditText FirstName , UserName , Email , Password , ConfirmPassword ;
+   Button regestir;
+   CheckBox term;
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        init();
+        ClickRegister();
 
+    }
+
+    private void ClickRegister() {
+     regestir.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             cheackable();
+         }
+     });
+   }
+
+    private boolean cheackable() {
+    if(FirstName.getText().toString().isEmpty())
+    {
+        Toast.makeText(getApplicationContext(),"FirstName is notFound",Toast.LENGTH_LONG).show();
+        return false;
+    }
+    else if (UserName.getText().toString().isEmpty())
+    {
+        Toast.makeText(getApplicationContext(),"UserName is notFound",Toast.LENGTH_LONG).show();
+        return false;
+    }
+    else if (Email.getText().toString().isEmpty())
+    {
+        Toast.makeText(getApplicationContext(),"Email is notFound",Toast.LENGTH_LONG).show();
+        return false;
+    }
+    else if(Password.getText().toString().isEmpty())
+    {
+        Toast.makeText(getApplicationContext(),"Password is notFound",Toast.LENGTH_LONG).show();
+        return false;
+    }
+    else if(ConfirmPassword.getText().toString().isEmpty())
+    {
+        Toast.makeText(getApplicationContext(),"ConfirmPassword is notFound",Toast.LENGTH_LONG).show();
+        return false;
+    }
+    else if (!term.isChecked())
+    {
+        Toast.makeText(getApplicationContext(),"terms is notFound",Toast.LENGTH_LONG).show();
+        return false;
+    }
+    else if (!ConfirmPassword.getText().toString().equals(Password.getText().toString()))
+    {
+        Toast.makeText(getApplicationContext(),"ConfirmPassword is Wrong",Toast.LENGTH_LONG).show();
+        return false;
+    }
+        return true;
+    }
+
+    private void init(){
+        FirstName = findViewById(R.id.firstName);
+        UserName = findViewById(R.id.UserName);
+        Email = findViewById(R.id.Email);
+        Password = findViewById(R.id.password_reg);
+        ConfirmPassword = findViewById(R.id.confirm_password);
+        regestir = findViewById(R.id.btn_reg);
+        term = findViewById(R.id.terms);
     }
 }
